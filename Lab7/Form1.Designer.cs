6f0ced9aa203a8b2ca55dc94b8f9486df1e73cbf
Александр Кулик <kartezhnik2012@mail.ru>
2025-03-25 18:58:32 +0300

    1

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.ShiftLeftButton = new System.Windows.Forms.Button();
+            this.ShiftRightButton = new System.Windows.Forms.Button();
+            this.FileLoadButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.FileSaveButton = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(39, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(372, 211);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 23;
+            this.listBox2.Location = new System.Drawing.Point(509, 39);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(372, 211);
+            this.listBox2.TabIndex = 1;
+            // 
+            // ShiftLeftButton
+            // 
+            this.ShiftLeftButton.Location = new System.Drawing.Point(283, 296);
+            this.ShiftLeftButton.Name = "ShiftLeftButton";
+            this.ShiftLeftButton.Size = new System.Drawing.Size(128, 37);
+            this.ShiftLeftButton.TabIndex = 2;
+            this.ShiftLeftButton.Text = "<<";
+            this.ShiftLeftButton.UseVisualStyleBackColor = true;
+            // 
+            // ShiftRightButton
+            // 
+            this.ShiftRightButton.Location = new System.Drawing.Point(509, 296);
+            this.ShiftRightButton.Name = "ShiftRightButton";
+            this.ShiftRightButton.Size = new System.Drawing.Size(119, 37);
+            this.ShiftRightButton.TabIndex = 3;
+            this.ShiftRightButton.Text = ">>";
+            this.ShiftRightButton.UseVisualStyleBackColor = true;
+            // 
+            // FileLoadButton
+            // 
+            this.FileLoadButton.Location = new System.Drawing.Point(509, 373);
+            this.FileLoadButton.Name = "FileLoadButton";
+            this.FileLoadButton.Size = new System.Drawing.Size(209, 37);
+            this.FileLoadButton.TabIndex = 4;
+            this.FileLoadButton.Text = "Загрузить из файла";
+            this.FileLoadButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(397, 450);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(119, 37);
+            this.CancelButton.TabIndex = 5;
+            this.CancelButton.Text = "Отмена";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // FileSaveButton
+            // 
+            this.FileSaveButton.Location = new System.Drawing.Point(202, 373);
+            this.FileSaveButton.Name = "FileSaveButton";
+            this.FileSaveButton.Size = new System.Drawing.Size(209, 37);
+            this.FileSaveButton.TabIndex = 6;
+            this.FileSaveButton.Text = "Сохранить в файл";
+            this.FileSaveButton.UseVisualStyleBackColor = true;
+            this.FileSaveButton.Click += new System.EventHandler(this.FileSaveButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 529);
+            this.Controls.Add(this.FileSaveButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.FileLoadButton);
+            this.Controls.Add(this.ShiftRightButton);
+            this.Controls.Add(this.ShiftLeftButton);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab 7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button ShiftLeftButton;
+        private System.Windows.Forms.Button ShiftRightButton;
+        private System.Windows.Forms.Button FileLoadButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button FileSaveButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Lab7/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ListBoxItems lbi = new ListBoxItems();
+
+            listBox1.DataSource = lbi.leftBoxItems;
+            listBox2.DataSource = lbi.rightBoxItems;
+        }
+
+        private void FileSaveButton_Click(object sender, EventArgs e)
+        {
+            // Вызываем диалог выбора файла
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            saveFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+
+            //ListBoxItems.LoadDataToFile(filename, listBox1.Items );
+        }
+    }
+}
